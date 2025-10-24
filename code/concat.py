2d11ec0d9ab649967f6d7de884fec58442b0b502
Gustavo Costa <theroyalplays@live.com>
2025-10-24 19:16:33 -0300

    feat(code): add concat

---
+++ code/concat.py
@@ -0,0 +1,7 @@
+# Progama que solicita duas entradas de texto e realiza concatenação.
+text1 = input("Digite o primeiro texto: ")
+text2 = input("Digite o segundo texto: ")
+
+text_concat = text1 + " " + text2
+
+print("Textos concatenados: " + text_concat)
